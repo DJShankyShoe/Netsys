d1b686faf339035c964c80917b9cd9699bd41835
Cirrus Vulpes <timothy.leezh@gmail.com>
2024-09-08 02:21:20 +0800

    test build

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 """silly little file for my silly little tests"""
 import random
-random.seed()
-if random.randint(0,1)==1:
-    print("hello worl")
-else:
-    print (">:3c")
-    
+def wow_a_test():
+    """hey idiot exclude docstring from the linter >:("""
+    random.seed()
+    if random.randint(0,1)==1:
+        print("hello worl")
+    else:
+        print (">:3c")
+wow_a_test()
+input("accept your fate?")
